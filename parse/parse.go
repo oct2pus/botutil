810a9a0363ca440ef392013883e2b8008ed239a3
Octopus Octopus <oct2pus@jade.moe>
2018-12-18 17:21:10 -0600

    move these back into their folders because packages are silly like that

---
+++ parse/parse.go
@@ -0,0 +1,18 @@
+package parse
+
+import (
+	"regexp"
+
+	"github.com/oct2pus/botutil/util/logging"
+)
+
+func CheckFormatted(input string, rgxp string) bool {
+	// todo: fix +- bullshit with regexp
+	compare, err := regexp.MatchString(rgxp, input)
+	logging.CheckError(err)
+
+	if compare {
+		return true
+	}
+	return false
+}
