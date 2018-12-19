c75f0bb840a5bf66e4d3648c7d2c0396456bf943
Octopus Octopus <oct2pus@jade.moe>
2018-12-18 18:13:43 -0600

    added embed and modified something in etc

--- embed/embed.go
+++ embed/embed.go
@@ -2,29 +2,47 @@ package embed
 
 import "github.com/bwmarrin/discordgo"
 
-func getCredits() *discordgo.MessageEmbed {
+func CreditsEmbed(botName string, artist string, artist2 string, artist3 string) *discordgo.MessageEmbed {
+
+	thanks := "Avatar by " + artist + "\n"
+
+	// this is such a hack
+	if artist2 != "" {
+		thanks += "Original Design by " + artist2 + "\n"
+	}
+
+	thanks += "Emojis by " + artist3
+
 	embed := &discordgo.MessageEmbed{
 		Color: 0x005682,
-		Type:  "A8out",
+		Type:  "About",
 		Fields: []*discordgo.MessageEmbedField{
 			&discordgo.MessageEmbedField{
-				Name:   "Vriska8ot",
-				Value:  "Created by \\🐙\\🐙#0413 ( http://oct2pus.tumblr.com/ )\nVriska8ot uses the 'discordgo' library\n( https://github.com/bwmarrin/discordgo/ )",
+				Name: botName,
+				Value: "Created by \\🐙\\🐙#0413" +
+					"( http://oct2pus.tumblr.com/ )\n" +
+					botName + " uses the 'discordgo' library\n" +
+					"( https://github.com/bwmarrin/discordgo/ )",
 				Inline: false,
 			},
 			&discordgo.MessageEmbedField{
 				Name:   "Special Thanks",
-				Value:  "Avatar By mjölk#8323 ( http://cosmic-rumpus.tumblr.com/ )\nEmojis by Dzuk#1671 ( https://noct.zone/ )",
+				Value:  thanks,
 				Inline: false,
 			},
 			&discordgo.MessageEmbedField{
-				Name:   "Disclaimer",
-				Value:  "Vriska8ot uses **Mutant Standard Emoji** (https://mutant.tech)\n**Mutant Standard Emoji** are licensed under CC-BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)",
+				Name: "Disclaimer",
+				Value: botName + " uses **Mutant Standard Emoji**" +
+					" ( https://mutant.tech )\n**Mutant Standard Emoji** are " +
+					" licensed under CC-BY-NC-SA 4.0 " +
+					"( https://creativecommons.org/licenses/by-nc-sa/4.0/ )",
 				Inline: false,
 			},
 		},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL: "https://raw.githubusercontent.com/oct2pus/vriskabot/master/art/avatar.png",
+			URL: "https://cdn.discordapp.com/" +
+				"avatars/497943811700424704/" +
+				"0a71258d950394d7adb4a369ae9f3722.png?size=1024",
 		},
 	}
 
